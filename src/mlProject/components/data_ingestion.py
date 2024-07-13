630ec260646e6c76dd0bdc6e160e4a5dc40ddf0c
rithin_prem <rithinengineeringmy@gmail.com>
2024-07-13 19:34:28 +0530

    Data ingestion and validation done

---
+++ src/mlProject/components/data_ingestion.py
@@ -0,0 +1,26 @@
+import os
+import urllib.request as request
+import zipfile
+from mlProject import logger
+from mlProject.utils.common import get_size
+from mlProject.entity.config_entity import DataIngestionConfig
+from pathlib import Path
+
+
+class DataIngestion:
+    def __init__ (self,config:DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename,headers = request.urlretrieve(
+                url = self.config.source_URL,
+                filename= self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with following info: \n{headers}")
+        else:
+            logger.info(f"File already exists of size {get_size(Path(self.config.local_data_file))}")
+
+
+    
+        
